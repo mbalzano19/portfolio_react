d785d05041fe036afcc1d5380a6d4ecd5ab2ca45
mbalzano19 <125160521+mbalzano19@users.noreply.github.com>
2023-09-25 20:09:02 -0400

    adds sign in page/welcome page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -10,24 +10,67 @@ import Projects from '../Projects/Projects'
 import BioPage from '../BioPage/BioPage'
 import { getUser } from '../../utilities/users-service'
 
-export default function App() {
+// export default function App() {
 
+//     const [user, setUser] = useState(null);
+
+// //   return (
+// //    <main className="App">
+
+
+
+// //         <NavBar />
+// //         <Routes>
+// //         <Route path='/home' element={<HomePage setUser={setUser} />} />
+// //           <Route path='/bio' element={<BioPage />} />
+// //           <Route path='/projects' element={<Projects />} />
+// //           <Route path='/contact' element={<Contact />} />
+// //         </Routes>
+
+
+// //    </main>
+// //   )
+// // }
+
+
+// return (
+//    <main className="App">
+//     {user ? (
+//       <>
+//         <NavBar />
+//         <Routes>
+//           {/* <Route path="/home" element={<HomePage />} /> */}
+//           <Route path='/bio' element={<BioPage />} />
+//           <Route path='/projects' element={<Projects />} />
+//           <Route path='/contact' element={<Contact />} />
+//       </>
+//     ) : (
+//       <Route path='/home' element={<HomePage setUser={setUser} />} />
+//     )}
+// </main>
+// );
+// }
+
+
+export default function App() {
+  const [user, setUser] = useState(null);
 
   return (
-   <main className="App">
-
-
-        <NavBar />
+    <main className="App">
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/bio" element={<BioPage />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </>
+      ) : (
         <Routes>
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/bio' element={<BioPage />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
+          <Route path="/" element={<HomePage setUser={setUser} />} />
         </Routes>
-
-
-   </main>
-  )
+      )}
+    </main>
+  );
 }
-
-
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,42 +1,37 @@
-import { checkToken } from '../../utilities/users-service'
-import './HomePage.css'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function HomePage() {
-  // async function handleCheckToken() {
-  //     const expDate = await checkToken()
-  //     console.log(expDate)
-  
-    return (
-      <>
-      <div className='biocontainer'>
-        <div className='left'>
-      <h1>Matthew Balzano</h1>
-      <h2 className='text-sm mt-20'>Software Engineer</h2>
-      <div className='picture'>
-      <img src="https://i.imgur.com/A6LO9td.jpg" alt="" id='headshot'/>
+
+export default function HomePage(props) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isUserSignedIn = props.user !== null;
+
+    if (isUserSignedIn) {
+      // If the user is signed in, redirect to the bio page
+      navigate("/");
+    }
+  }, [props.user, navigate]);
+
+  const handleSignIn = () => {
+    // You can set a default username or customize this based on your needs
+    const defaultUsername = "Guest"; // Change this to the desired default username
+    props.setUser(defaultUsername);
+    navigate("/bio");
+  };
+
+  return (
+    <div className="full-screen-container">
+      <div className="login-container">
+        <h3 className="login-title">Welcome to My Portfolio</h3>
+        <button
+          onClick={handleSignIn}
+          className="login-button"
+        >
+          Sign In
+        </button>
       </div>
-      </div>
-      <div className="md:container md:mx-auto z-10 w-full max-w-5xl items-center justify-between flex-col font-mono text-sm lg:flex mt-20" id='bio'>
-        <p>In early 2023, I decided to chase a passion of mine: software engineering. Wanting to push myself to better my skills, I enrolled in General Assembly's Software Engineering Immersive Bootcamp in April of 2023. During this experience, I built full-stack applications and deepended my knowledge of JavaScript and Python. Additionally, I learned some new skills, such as React.
-        <br />
-        <br />
-        I come from the audio tech industry where I worked as a Customer Support Specialist for three years. Succeeding in this role allowed me to walk away with a deep love for creatively problem solving, as well as connecting with customers to understand their needs and solving their problems. This is largely what lead me to the software engineering industry: to be able to creatively solve problems using code.
-        <br />
-        <br />
-        I am currently looking for my first software development role. Although I do not have the most experience, I do have an unwavering tenacity to consistently learn and develop my skills. One thing my bootcamp experience taught me is how to learn complex topics in a short period of time. I can't wait for the opportunity to bring my knowledge and desire for learning to a company.
-        <br />
-        <br />
-        When I am not working, you can catch me working out in the gym, hiking a mountain, or making music in the studio. I am passionate about personal health and I love to express myself creatively.
-
-        </p>
-      </div>
-      </div>
-      </>
-    )
-  }
-  
-
-
-  
-
-  
+    </div>
+  );
+}
